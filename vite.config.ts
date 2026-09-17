b5c5b4cb4987e3aae6378c85a0748fcbf93c51ef
saurabh pawar <jussaurabh@saurabhs-MacBook-Pro.local>
2026-09-17 23:59:58 +0530

    chore: setup github pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/jussaurabh/', // Required for GitHub Pages deployment (repo name)
   plugins: [react(), tailwindcss()],
 })
